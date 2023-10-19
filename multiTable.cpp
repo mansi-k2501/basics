fa021a4768c419a85ad4cf8d92d0c868e0a20823
Mansi Kanakamedala <140502784+mansi-k2501@users.noreply.github.com>
2023-10-19 19:29:05 +0530

    Add files via upload

---
+++ multiTable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int a,i ;
+    cout<<"Multiplication Table";
+    cout<<"enter a number:";
+    cin>>a;
+    i=1;
+    while(i<=20)
+    {
+        cout<<a<<"x"<<i<<"= "<<i*a<<endl;
+        i++;
+    }
+}
